ea6854a026cf46033cf1e0376275839a230ac9b1
stanley <chw058@gmail.com>
2017-10-22 08:01:13 -0700

    Hello world

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var express = require("express-handlebars");
+var exphbs = require("express-handlebars");
+
+var port = 3000;
 
 var app = express();
 
+//console.log(app);
+
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded( {extended: false} ));
 
-var exphbs = require("express-handlebars");
+
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.get("/", function(req, res){
+
+    res.render("index", {});
+
+})
+
+app.listen(port, function(error){
+
+    if (error) {
+
+        return console.log(error);
+
+    }
+
+    console.log("listening on port: " + port);
+
+})
 
